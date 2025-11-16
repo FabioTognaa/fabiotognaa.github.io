3ee17d403419b1a45ae9c1743f984a14a53c9a4d
Fabio Tognarelli <fabiotognaa@gmail.com>
2025-11-16 18:00:58 +0100

    creazione dist - gestione percorso dir online

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Fabio_Tognarelli_WebSite/",
   plugins: [tailwindcss()],
 });
